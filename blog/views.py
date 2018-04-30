4ea135938acfc44e579d444e144fee2bbd40f713
Shaun Greene <shaun@spgreene.com>
2018-04-30 20:08:06 +0000

    Add post draft list view/page

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,11 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
