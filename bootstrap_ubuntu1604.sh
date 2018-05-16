b08f12d14d8b567c5b2be2ed53a0b7311538f454
Shaun <shaun@spgreene.com>
2018-05-16 16:32:21 -0400

    Add additional flags to curl
    to better support headless scripts.

--- bootstrap_ubuntu1604.sh
+++ bootstrap_ubuntu1604.sh
@@ -13,8 +13,8 @@ apt-get install -y python3-venv
 snap install atom --classic
 
 # install CodeClimate CLI
-curl -L --anyauth https://github.com/codeclimate/codeclimate/archive/master.tar.gz | tar xvz
+curl -fsSL --anyauth https://github.com/codeclimate/codeclimate/archive/master.tar.gz | tar xvz
 cd codeclimate-* && sudo make install
 
 # install Krypton for authenticating SSH and GPG
-curl -L --anyauth https://krypt.co/kr | sh
+curl -fsSL --anyauth https://krypt.co/kr | sh
